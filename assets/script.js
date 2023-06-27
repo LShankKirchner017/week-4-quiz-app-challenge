ef5bab5769cc437445693b2bee1556c11d2932f4
Lindsay <lshank017@gmail.com>
2023-06-27 11:06:08 -0500

    removed parts of JS logic

--- assets/script.js
+++ assets/script.js
@@ -1,74 +1,62 @@
-//what do we need to make this run?
-var options = document.getElementById("options")
-var userSelection = document.getElementById("userSelection")
-var quizIndex = 0
-var start = document.querySelector(".start-button")
+//what variables do we need to make this run?
+var quizQuestion = document.getElementById("quizQuest");
+var quizOptions = document.getElementById("options");
+var userChoice = document.getElementById("userSelection")
 // press start button 
-
+function(){
+    
+}
 // start timer
-
-// ask the question & show the options
-
-var questAsk = document.getElementById("quizQuest");
-var choiceA = document.getElementById("a")
-var choiceB = document.getElementById("b");
-var choiceC = document.getElementById("c");
-var choiceD = document.getElementById("d");
-// store userSelection
 
 // build a question bank
 var questions = [
-  {
-    question: "What does HTML stand for?",
-    a: "Hot Text Markup Link",
-    b: "How To Markup Language",
-    c: "Hypertext Markup Language",
-    d: "Hypertext Makeup Language",
-    correct: "c",
-  },
-
-  {
-    question: "What does CSS stand for?",
-    a: "Cascading Style Sheet",
-    b: "Coding Style Sheet",
-    c: "Cascading Sheet Style",
-    d: "Cool Style Sheet",
-    correct: "a",
-  },
-
-  {
-    question: "What do we use to make a website interactive?",
-    a: "Script",
-    b: "Coffee",
-    c: "Java",
-    d: "JavaScript",
-    correct: "d",
-  },
-
-  {
-    question: "What are the primative types",
-    a: "Boolean, String, Number, Button",
-    b: "String, Number, Boolean, Undefined",
-    c: "Logic, Math, Random, Comparison",
-    d: "None of the above",
-    correct: "b",
-  },
+    {
+        question: "What does HTML stand for?",
+        a: "Hot Text Markup Link",
+        b: "How To Markup Language",
+        c: "Hypertext Markup Language",
+        d: "Hypertext Makeup Language",
+        correct: "c",
+    },
+    
+    {
+        question: "What does CSS stand for?",
+        a: "Cascading Style Sheet",
+        b: "Coding Style Sheet",
+        c: "Cascading Sheet Style",
+        d: "Cool Style Sheet",
+        correct: "a",
+    },
+    
+    {
+        question: "What do we use to make a website interactive?",
+        a: "Script",
+        b: "Coffee",
+        c: "Java",
+        d: "JavaScript",
+        correct: "d",
+    },
+    
+    {
+        question: "What are the primative types",
+        a: "Boolean, String, Number, Button",
+        b: "String, Number, Boolean, Undefined",
+        c: "Logic, Math, Random, Comparison",
+        d: "None of the above",
+        correct: "b",
+    },
 ];
 // do this over and over 
 
 // show the quiz 
 
-
+// ask the question & show the options
 
 // remove time for incorrect answers
 
-
-
 // end quiz
 
-
 // end highscore & initials
-
 
 
 // locally store initials & score
