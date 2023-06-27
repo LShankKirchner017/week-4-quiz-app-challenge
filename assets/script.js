6a6f1d1a67287a5afada56bfcbe89745a4a40ea5
Lindsay <lshank017@gmail.com>
2023-06-27 09:09:17 -0500

    started pseudocode in JS; built question bank

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,79 @@
+//what do we need to make this run?
+// press start button 
+
+
+// start timer
+
+
+
+// ask the question & show the options
+var questAsk = document.getElementById("quizQuest")
+var choiceA = document.getElementById("a")
+var choiceB = document.getElementById("b");
+var choiceC = document.getElementById("c");
+var choiceD = document.getElementById("d");
+// store userSelection
+
 // build a question bank
 var questions = [
-    {
-        question: "",
-        optionA: "", 
-        correctOption: "", 
-    }, 
+  {
+    question: "What does HTML stand for?",
+    a: "Hot Text Markup Link",
+    b: "How To Markup Language",
+    c: "Hypertext Markup Language",
+    d: "Hypertext Makeup Language",
+    correct: "c",
+  },
 
-]
+  {
+    question: "What does CSS stand for?",
+    a: "Cascading Style Sheet",
+    b: "Coding Style Sheet",
+    c: "Cascading Sheet Style",
+    d: "Cool Style Sheet",
+    correct: "a",
+  },
+
+  {
+    question: "What do we use to make a website interactive?",
+    a: "Script",
+    b: "Coffee",
+    c: "Java",
+    d: "JavaScript",
+    correct: "d",
+  },
+
+  {
+    question: "What are the primative types",
+    a: "Boolean, String, Number, Button",
+    b: "String, Number, Boolean, Undefined",
+    c: "Logic, Math, Random, Comparison",
+    d: "None of the above",
+    correct: "b",
+  },
+];
+// do this over and over 
+
+// show the quiz 
+function displayQuiz (){}
 
 
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and my score
+// remove time for incorrect answers
+
+
+
+// end quiz
+
+
+// end highscore & initials
+
+
+
+// locally store initials & score
+
+
+
+
+
+
+
